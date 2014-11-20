cd70340d2b8870881be95caed76e2ac23d4258b1
Marieke van Erp <marieke.van.erp@vu.nl>
2014-11-20 13:56:10 +0100

    Added Levenshtein version
    This version limits the edit distance between the candidates and the
    entity text

---
+++ entity_reranker_withLevenshtein.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+## This script takes a naf file with an entities layer as input 
+## For each entity disambiguation candidate, it looks up the dbpedia type (class)
+## of each candidate and reranks the candidates based on the dbpedia class ranking 
+## for the domain at hand. 
+## This version loads the dbpedia resource table in memory
+## It also only prints the highest ranking candidate 
+## V1.001 This version also computes the Levenshtein distance and reranks the candidates accordingly 
+
+## Created by: Marieke van Erp (marieke.van.erp@vu.nl)
+## Date: 20 November 2014 
+
+import sys
+from KafNafParserPy import *
+import codecs
+import re
+import datetime
+from random import randrange
+from Levenshtein import distance
+
+# Resource Types table 
+dbpedia_table =  "DBpediaResourceTypeTableOnlyRanked.tsv"
+
+lp = Clp()
+lp.set_name('vua-nedtype-reranking-levenshtein')
+lp.set_version('1.1')
+lp.set_beginTimestamp()
+
+types = {}
+with open(dbpedia_table) as f:
+	for line in f:
+		line = re.sub('\n', '',line)
+		fields = line.split('\t')
+		types[fields[0]] = fields[1]
+
+# Get the file
+input = sys.stdin
+
+# parse the XML
+my_parser = KafNafParser(input)
+
+# Get the entities 
+for entity in my_parser.get_entities():
+	terms = []
+	entity_text = "" 
+	# Get the span of the references (entities) 
+	for reference in entity.get_references():
+		for span_obj in reference:
+			for item in span_obj.get_span_ids():
+				terms.append(item)
+	# Also get the text that denote the entities 
+	for term in terms:
+		entity_text = entity_text + " " + my_parser.get_term(term).get_lemma()
+	entity_text = entity_text.rstrip()
+	reranked = {}
+	# Also get the actual references 
+	for external_reference in entity.get_external_references():
+		resource_name = external_reference.get_reference().replace('http://dbpedia.org/resource/','')	
+		if resource_name in types and external_reference.get_resource() == 'spotlight_v1' and distance(entity_text,resource_name) < 10:
+			reranked[external_reference.get_reference()] = types[resource_name]
+	if len(reranked) > 0:
+		max_key, value = max(reranked.iteritems(), key=lambda x:x[1])
+		new_reference = CexternalReference()			
+		new_reference.set_resource('vua-type-reranker-v1.1')
+		new_reference.set_reference(max_key)
+		#new_reference.set_reference(external_reference.get_reference())
+		new_reference.set_confidence(types[resource_name])
+			#print entity_text, entity.get_type(), external_reference.get_reference(), types[resource_name], distance(entity_text,resource_name)
+		my_parser.add_external_reference_to_entity(entity.get_id(),new_reference)
+			
+
+lp.set_endTimestamp()
+my_parser.add_linguistic_processor('entities', lp)
+
+my_parser.dump()
+		
+
